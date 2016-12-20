a47b14853e90c07b28566c1c21d7fe95e0b3f563
Parad0xPl <paradxpl@gmail.com>
2016-12-20 18:55:30 +0100

    Added translation to weather

---
+++ public/translations/frontend/translation_pl.js
@@ -0,0 +1,6 @@
+succesMessage = Pomyślnie zrealizowane.
+weather_current = Aktualne warunki
+weather_underground_languagecode = EN
+feelsLike = Odczuwalna
+error_weather =  Proszę ustawić większe miasto.
+confirmMessage = Czy jesteś pewny? Tego nie można cofnąć.
