c155ded86a78804dc0e13d66d9b3bd3c030c205a
Parad0xPl <paradxpl@gmail.com>
2017-01-30 17:02:42 +0100

    dashboard:   fixed style

--- public/app/tile.directive.js
+++ public/app/tile.directive.js
@@ -9,7 +9,9 @@ angular
       link: function (scope, el) {
         // el.addClass('tile')
         scope.size = scope.item.tileSize || scope.size;
-        el.addClass(`col-md-${scope.size * 2}`);
+        el.addClass(`col-md-${(scope.size * 3)%13}`);
+        el.addClass(`col-sm-${(scope.size * 4)%13}`);
+        el.addClass(`col-sm-${(scope.size * 6)%13}`);
         el.attr('style', scope.item.tileCSS);
       },
       controller: function ($scope) {
